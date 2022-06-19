919546eb59b3fd565e45060ab3a53b08b2470f19
Matthew Henderson <info@matthewahenderson.com>
2022-06-19 14:43:39 -0400

    Completed project styling and generation of secret message; deployed using Vercel

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
-    const input = document.querySelector('#message-input');
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
 
-    console.log(input.value);
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input');
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
 })
